98d6860f9a739f60dce498d1de41ddfe75f8aca5
Philipp <philipp98.joos@gmail.com>
2016-05-30 22:15:28 +0200

    Update totp.py

--- totp.py
+++ totp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*
 '''
 The MIT License (MIT)
-Copyright (c) 2015 Philipp Joos
+Copyright (c) 2016 Philipp Joos
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
 deal in the Software without restriction, including without limitation the rights
