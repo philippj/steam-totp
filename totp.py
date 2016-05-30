e80298257ce0896ba0d4e39da139e6ca9614f7e2
Philipp <philipp98.joos@gmail.com>
2016-05-30 22:14:58 +0200

    Update totp.py

--- totp.py
+++ totp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*
 '''
 The MIT License (MIT)
-Copyright (c) 2015 Michael Peters
+Copyright (c) 2015 Philipp Joos
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to
 deal in the Software without restriction, including without limitation the rights
@@ -147,7 +147,6 @@ class SteamTOTP:
         if 'deviceID' not in self.secrets:
             return self.generateDeviceID(steamID)
         else:
-            print 'this.'
             return self.secrets['deviceID']
 
     def generateDeviceID(self, steamID=False, prefix='android'):
